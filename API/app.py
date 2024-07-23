82b7b63ef8e9fbd821279e0e47fefd61da3799b3
Luis Espinoza <luisespinozanav024@gmail.com>
2024-07-23 12:43:57 -0500

    segunda versión funcional modelo-api-frontend local

--- API/app.py
+++ API/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 import os
+import zipfile
 from utils import separate_tracks, extract_filename
 
 app = FastAPI()
@@ -15,21 +16,32 @@ class SeparateRequest(BaseModel):
 
 @app.post('/separate')
 def separate_file(request: SeparateRequest):
-
-
     wav_file = request.wav_file
-
     file_path = os.path.join(UPLOADS_PATH, wav_file)
 
     if not os.path.isfile(file_path):
         raise HTTPException(status_code=404, detail="File not found")
 
-    # Extraer el nombre base del archivo
-    base_name = extract_filename(wav_file)
-
     # Separar las pistas
     separate_tracks(file_path, SEPARATED_FILES_PATH)
 
-    output_directory = os.path.join(SEPARATED_FILES_PATH, base_name)
+    output_directory = os.path.join(SEPARATED_FILES_PATH, extract_filename(wav_file))
+    zip_filename = f"{extract_filename(wav_file)}_separated.zip"
+    zip_file_path = os.path.join(SEPARATED_FILES_PATH, zip_filename)
 
-    return {"detail": "Separation done", "output_directory": output_directory}
+    # Crear el archivo ZIP usando zipfile
+    with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+        for root, _, files in os.walk(output_directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, output_directory)
+                zipf.write(file_path, arcname=arcname)
+
+    return {"detail": "Separation done", "output_directory": output_directory, "zip_filename": zip_filename}
+
+@app.get('/download/{filename}')
+def download_file(filename: str):
+    file_path = os.path.join(SEPARATED_FILES_PATH, filename)
+    if not os.path.isfile(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    return StreamingResponse(open(file_path, 'rb'), media_type='application/zip', headers={'Content-Disposition': f'attachment; filename="{filename}"'})
--- frontend/front.py
+++ frontend/front.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import os
 import requests
-from io import BytesIO
-import zipfile
 
-# Ruta donde se guardarán los archivos subidos
 UPLOADS_PATH = "/home/luis/code/pbn327/SoundSlice/frontend/uploaded_files"
 SEPARATED_FILES_PATH = "/home/luis/code/pbn327/SoundSlice/frontend/separated_files"
 
@@ -33,46 +30,13 @@ if uploaded_file:
 
     # Llamar a la API para separar las pistas
     response = requests.post('http://localhost:8000/separate', json={'wav_file': uploaded_file.name})
+
     if response.status_code == 200:
-        st.success("Pistas separadas correctamente. Selecciona las pistas para descargar.")
+        st.success("Pistas separadas correctamente.")
+
+        zip_filename = response.json().get('zip_filename')
+        zip_file_url = f'http://localhost:8000/download/{zip_filename}'
+
+        st.markdown(f"[Descargar archivo ZIP]({zip_file_url})", unsafe_allow_html=True)
     else:
         st.error("Hubo un error al procesar la canción.")
-
-    st.header("Pistas Disponibles")
-    track_folder = uploaded_file.name.replace('.wav', '')
-    track_folder_path = os.path.join(SEPARATED_FILES_PATH, track_folder)
-
-    # Nombres de las pistas esperadas
-    expected_tracks = ["bass.wav", "drums.wav", "other.wav", "vocals.wav"]
-
-    if os.path.exists(track_folder_path):
-        print("probando1")
-        track_files = [f for f in expected_tracks if os.path.isfile(os.path.join(track_folder_path, f))]
-        selected_tracks = []
-
-        for track_file in track_files:
-            if st.checkbox(track_file):
-                selected_tracks.append(os.path.join(track_folder_path, track_file))
-
-        print("probando2")
-        # En la sección de descarga de pistas seleccionadas
-
-        if selected_tracks:
-            print("probando3")
-            zip_path = os.path.join(SEPARATED_FILES_PATH, f"{track_folder}_selected_tracks.zip")
-
-            with zipfile.ZipFile(zip_path, 'w') as zipf:
-                print("probando4")
-                for file in selected_tracks:
-                    zipf.write(file, os.path.basename(file))
-
-            with open(zip_path, 'rb') as f:
-                print("probando5")
-                st.download_button(
-                    label="Descargar Pistas Seleccionadas",
-                    data=f.read(),
-                    file_name=f"{track_folder}_selected_tracks.zip",
-                    mime="application/zip",
-                )
-        else:
-            st.write("Selecciona las pistas que deseas descargar.")
